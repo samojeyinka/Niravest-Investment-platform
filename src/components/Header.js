2fc88310d53b1088eba7dbb5eeea598ffeebca23
samojeyinka <samuelojeyinka@gmail.com>
2024-02-26 13:47:08 -1200

    dynamic menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ import '../stylesheets/Header.css'
 const Header = () => {
 
     const [open,setOpen] = useState(false);
+    const [click,setClick] = useState(false);
+    
+    const handleClick = () => {
+        setClick(!click);
+    }
+
     let dropdown = useRef();
 
     useEffect(() => {
@@ -34,7 +40,20 @@ const Header = () => {
                 <img src={logo} className='logo_img'/>
                 <span className='logo_txt'>niravest</span>
             </div>
-            <div className='navbar_middle'>
+
+            <div className='hamburger' onClick={handleClick}>
+
+
+            <div className={click ? 'bars-div open bar1 open bar2 open bar3' : 'bars-div'}>
+              <div className='bar1'></div>
+              <div className='bar2'></div>
+              <div className='bar3'></div>
+
+            </div>
+          </div>
+
+
+            <div className={`navbar_middle ${click ? 'active' : ''}`}>
                 <ul className='menu_links'>
                     <li>
                         <Link to={'/'}>
@@ -51,6 +70,17 @@ const Header = () => {
                     <li>
                         <Link to={'/'}>
                             Packages
+                        </Link>
+                    </li>
+
+                    <li className='mobile-only-link'>
+                        <Link to={'/'}>
+                            Sign In
+                        </Link>
+                        </li>
+                        <li className='mobile-only-link'>
+                        <Link to={'/'}>
+                            Sign Up
                         </Link>
                     </li>
 
