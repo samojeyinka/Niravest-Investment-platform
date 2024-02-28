b870aaf4650dca2f3a5a1688cbebbad3d96d7370
samojeyinka <samuelojeyinka@gmail.com>
2024-02-27 14:12:37 -1200

    modified the attributes section

--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -7,13 +7,10 @@ const Attributes = () => {
             <div className='attributes_flex'>
                 <div className='attributes_left'>
                     <span>GET STARTED IN MINUTES</span>
-                    <h4>3 Main Reaosn to
-                        Choose us.</h4>
+                    <h4>Three Main Reasons to Choose Us.</h4>
                     <img src={divider} />
-                    <h5>Register and create your first
-                        support portal</h5>
-                    <p>It only takes 5 minutes. Set-up is smooth & simple,
-                        with fully customisable page design to reflect your brand lorem dummy.</p>
+                    <h5>Register and activate your first investent package</h5>
+                    <p>Begin Your Investment Journey: Register and Activate Your First Investment Package Today for a Secure Financial Future.</p>
                         <h5 className='add_line'>Manage your dashbaord easily</h5>
                         <h5 className='add_line'>Start Investing</h5>
                 </div>
