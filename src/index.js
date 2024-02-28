8829b30132cf71d5286c4e9825bebba0dc8fa8cd
samojeyinka <samuelojeyinka@gmail.com>
2024-02-27 16:58:23 -1200

    fixed bug

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap";
+
 
 
 
